e688657c0da0b846efcb697e82350f0f967ad728
goalizc <goalizc@DESKTOP-HCR1HL2.localdomain>
2026-09-04 20:38:21 +0800

    GCS_MAVLink: add MSG_SHOW_CLOCK survey message id
    D6 clock survey (DATA32 report of per-vehicle GPS epoch and internal
    clock) reuses the ap_message push mechanism used by MSG_SHOW_STATUS.

--- libraries/GCS_MAVLink/ap_message.h
+++ libraries/GCS_MAVLink/ap_message.h
@@ -117,5 +117,6 @@ enum ap_message : uint8_t {
     MSG_UTM_GLOBAL_POSITION            = 101,
 #endif  // AP_MAVLINK_UTM_GLOBAL_POSITION_SENDING_ENABLED
     MSG_SHOW_STATUS                    = 102,
+    MSG_SHOW_CLOCK                     = 103,
     MSG_LAST // MSG_LAST must be the last entry in this enum
 };
